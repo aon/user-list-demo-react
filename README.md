5a8d0bc36ede493b5cfd1f423b277ccd04066024
aon <21188659+aon@users.noreply.github.com>
2022-02-07 19:38:28 -0300

    First upload

---
+++ README.md
@@ -0,0 +1,20 @@
+# Userlist - Demo - React
+This small website will get a username and age, verify the input is valid and add it to a list shown at the bottom of the page.
+
+The main learning goals for this project were:
+- Learn how modals work
+- Learn how to manage different states
+- Keep improving my React skills
+
+## Screenshots
+![Home page](images/home.png "Home page")
+![Modal page](images/modal.png "Modal page")
+
+## Built with
+- JavaScript
+- HTML
+- CSS
+- React
+
+## Authors
+- Agustin Aon - @aon
---
+++ src/components/Users/AddUser.js
@@ -0,0 +1,69 @@
+import React, { useState, useRef } from "react";
+
+import Button from "../UI/Button";
+import Card from "../UI/Card";
+import ErrorModal from "../UI/ErrorModal";
+import classes from "./AddUser.module.css";
+
+const AddUser = (props) => {
+  const nameInputRef = useRef();
+  const ageInputRef = useRef();
+
+  const [error, setError] = useState();
+
+  const addUserHandler = (event) => {
+    event.preventDefault();
+
+    const enteredUsername = nameInputRef.current.value;
+    const enteredAge = ageInputRef.current.value;
+
+    if (enteredUsername.trim().length === 0 || enteredAge.trim().length === 0) {
+      setError({
+        title: "Invalid input",
+        message: "Please enter a valid name and age.",
+      });
+      return;
+    }
+
+    if (+enteredAge < 1) {
+      // +casts to number
+      setError({
+        title: "Invalid input",
+        message: "Please enter a valid age.",
+      });
+      return;
+    }
+
+    props.onAddUser(enteredUsername, enteredAge);
+
+    nameInputRef.current.value = "";
+    ageInputRef.current.value = "";
+  };
+
+  const errorHandler = () => {
+    setError(null);
+  };
+
+  return (
+    <React.Fragment>
+      {error && (
+        <ErrorModal
+          title={error.title}
+          message={error.message}
+          onConfirm={errorHandler}
+        />
+      )}
+      <Card className={classes.input}>
+        <form onSubmit={addUserHandler}>
+          <label htmlFor="username">Username</label>
+          <input id="username" type="text" ref={nameInputRef} />
+          <label htmlFor="age">Age (Years)</label>
+          <input id="age" type="number" ref={ageInputRef} />
+          <Button type="submit">Add User</Button>
+        </form>
+      </Card>
+    </React.Fragment>
+  );
+};
+
+export default AddUser;
